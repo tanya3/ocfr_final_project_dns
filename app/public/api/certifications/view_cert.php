49c1871074e83f4f23d3300b9d2d0a2d8bbfcba3
zxcv5853 <37623021+zxcv5853@users.noreply.github.com>
2019-10-17 19:15:22 -0400

    Certications Working

--- app/public/api/certifications/view_cert.php
+++ app/public/api/certifications/view_cert.php
@@ -10,7 +10,7 @@ $db = DbConnection::getConnection();
 $certifications = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($certifactions, JSON_PRETTY_PRINT);
+$json = json_encode($certifications, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
