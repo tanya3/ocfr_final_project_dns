6f170cfd7965db3505f5c0ad3b42b3f8f964a46e
zxcv5853 <37623021+zxcv5853@users.noreply.github.com>
2019-10-17 18:19:58 -0400

    view certifications
    needed for viewing

---
+++ app/public/js/certificationView.app.js
@@ -0,0 +1,19 @@
+var memberViewApp = new Vue({
+  el: '#certificationViewApp',
+  data: {
+    certifications: [],
+  },
+  methods: {
+    fetchCertifications() {
+      fetch('api/certifications/')
+      .then(response => response.json())
+      .then(json => { certificationViewApp.certifications = json })
+    },
+    handleRowClick(certification) {
+      this.certification=certification;
+    }
+  },
+  created() {
+    this.fetchCertifications();
+  }
+});
