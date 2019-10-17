3bef57f3647524ab51b95872b128772192a4c512
Tanya Gupta <tanya.gupta200@gmail.com>
2019-10-16 23:50:13 -0400

    Added docker and changed database env

---
+++ app/data/schema-ocfr.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Members (
+  memberId int NOT NULL AUTO_INCREMENT,
+  firstName varchar(255),
+  lastName varchar(255),
+	address varchar(255),
+  email varchar(255),
+	phoneNumber varchar(255),
+	dob varchar(255),
+	gender varchar(255),
+	isActive boolean,
+	radioNumber varchar(255),
+  stationNumber varchar(255),
+	PRIMARY KEY(memberId)
+);
+
+CREATE TABLE Certifications (
+  certId int,
+  certName varchar(255),
+  stdExp int,
+ 	PRIMARY KEY(certId)
+);
+
+CREATE TABLE MemberCert (
+  memberId int,
+  certId int,
+	startDate date,
+ 	endDate boolean,
+	FOREIGN KEY (memberId) REFERENCES Members(memberId),
+	FOREIGN KEY (certId) REFERENCES Certifications(certId)
+);
