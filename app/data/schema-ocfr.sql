601a6d47e8510f8c01fbf9da210c8418b2f80a60
Tanya Gupta <tanya.gupta200@gmail.com>
2019-10-20 11:23:29 -0400

    New DB schema and filter

--- app/data/schema-ocfr.sql
+++ app/data/schema-ocfr.sql
@@ -1,16 +1,17 @@
 CREATE TABLE Members (
-  memberId int NOT NULL AUTO_INCREMENT,
-  firstName varchar(255),
-  lastName varchar(255),
-	address varchar(255),
-  email varchar(255),
-	phoneNumber varchar(255),
-	dob varchar(255),
-	gender varchar(255),
-	isActive boolean,
-	radioNumber varchar(255),
-  stationNumber varchar(255),
-	PRIMARY KEY(memberId)
+	memberId varchar(50),
+	firstName varchar(255),
+	lastName varchar(255),
+	member_address varchar(255),
+	member_email varchar(255),
+	member_phoneNumber bigint,
+	member_dob date,
+	member_gender varchar(255),
+	member_status varchar(10),
+	radioNumber varchar(10),
+	stationNumber int,
+	PRIMARY KEY(memberId),
+    constraint c1 CHECK (member_status in ('Active', 'Inactive'))
 );
 
 CREATE TABLE Certifications (
@@ -22,12 +23,11 @@ CREATE TABLE Certifications (
 );
 
 CREATE TABLE MemberCert (
-  memberId int,
-  certId int,
+  memberId varchar(50),
+  certId varchar(50),
 	startDate date,
- 	endDate boolean,
-	FOREIGN KEY (memberId) REFERENCES Members(memberId),
-	FOREIGN KEY (certId) REFERENCES Certifications(certId)
+ 	endDate date,
+	FOREIGN KEY (memberId) REFERENCES Members(memberId) ON DELETE CASCADE,
+	FOREIGN KEY (certId) REFERENCES Certifications(certId) ON DELETE CASCADE,
+  PRIMARY KEY (memberId, certId)
 );
-
-SELECT * FROM Members;
--- app/public/js/expCert.app.js
+++ app/public/js/expCert.app.js
@@ -2,12 +2,21 @@ var expCertApp = new Vue({
   el: '#expCertApp',
   data: {
     member_cert: [],
+    filter: {
+      cert: ''
+    },
+    certifications: []
   },
   methods: {
     fetchExpCert() {
       fetch('api/members/fetchExpCert.php')
       .then(response => response.json())
       .then(json => { expCertApp.member_cert = json });
+    },
+    fetchCert() {
+      fetch('api/certifications/')
+      .then(response => response.json())
+      .then(json => { expCertApp.certifications = json })
     },
     certActive(endDate) {
       if(moment(endDate)<moment()) {
@@ -20,5 +29,6 @@ var expCertApp = new Vue({
   },
   created() {
     this.fetchExpCert();
+    this.fetchCert();
   }
 });
