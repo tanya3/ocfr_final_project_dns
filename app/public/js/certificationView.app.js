c52e2c7f256ea6ff258bfd583282d95dce7d7675
Heilman, Philip Raymond <pheilman@iu.edu>
2019-10-20 15:26:35 -0400

    Delete Certification
    Adding functionality to delete certifications

--- app/public/js/certificationView.app.js
+++ app/public/js/certificationView.app.js
@@ -13,6 +13,21 @@ var certificationViewApp = new Vue({
       this.certification=certification;
     }
   },
+  handleDeleteCert(c) {
+    this.deleteCert=c;
+    fetch('api/certifications/certDelete.php', {
+      method:'POST',
+      body: JSON.stringify(this.deleteCert),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    .then(response => response.json())
+    .then(json => { certificationViewApp.certifications = json })
+    .then(response => {alert('Deleted!')})
+    this.handleReset();
+  }
+},
   created() {
     this.fetchCertifications();
   }
