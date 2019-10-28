19654c27df3f899614749c6b9e693cf5dd57814f
Tanya Gupta <tanya.gupta200@gmail.com>
2019-10-27 20:03:45 -0400

    CSV FIle download

--- app/public/js/expCertReport.app.js
+++ app/public/js/expCertReport.app.js
@@ -26,6 +26,56 @@ var expCertReportApp = new Vue({
         return 1;
       }
     },
+    convertArrayOfObjectsToCSV(args) {
+        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+        data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+    },
+    downloadCSV(args) {
+        var data, filename, link;
+
+        var csv = this.convertArrayOfObjectsToCSV({
+            data: this.member_cert
+        });
+        if (csv == null) return;
+
+        filename = args.filename || 'export.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    }
   },
   created() {
     this.fetchExpCert();
